95ba651b53dee2fa3ebb85a179a8b809c1515435
Chamika Dilshan <it22118318@my.sliit.lk>
2025-04-28 23:54:47 +0530

    8443 add

--- foodsera-app/src/lib/api.ts
+++ foodsera-app/src/lib/api.ts
@@ -2,13 +2,13 @@ import { Cart, CartItem, MenuItem, Order, Restaurant, User } from './types';
 
 // Base API URLs for different services
 const API_URLS = {
-  auth: 'https://foodix.dynac.space/auth-proxy/api',
-  admin: 'https://foodix.dynac.space/admin-proxy/api',
-  delivery: 'https://foodix.dynac.space/delivery-proxy/api',
-  order: 'https://foodix.dynac.space/order-proxy/api',
-  payment: 'https://foodix.dynac.space/payment-proxy/api',
-  restaurant: 'https://foodix.dynac.space/restaurent-proxy/api',
-  notification: 'https://foodix.dynac.space/notification-proxy/api',
+  auth: 'https://foodix.dynac.space:8443/auth-proxy/api',
+  admin: 'https://foodix.dynac.space:8443/admin-proxy/api',
+  delivery: 'https://foodix.dynac.space:8443/delivery-proxy/api',
+  order: 'https://foodix.dynac.space:8443/order-proxy/api',
+  payment: 'https://foodix.dynac.space:8443/payment-proxy/api',
+  restaurant: 'https://foodix.dynac.space:8443/restaurent-proxy/api',
+  notification: 'https://foodix.dynac.space:8443/notification-proxy/api',
 };
 
 // Get token from local storage
